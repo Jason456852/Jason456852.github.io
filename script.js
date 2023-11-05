7f6fd56bde20028da9f4e39414b70c2b96f84567
Jason456852 <pakling.yeung456852@gmail.com>
2023-11-05 12:35:10 -0700

    new file:   .gitignore 	modified:   README.md 	deleted:    contact/index.html 	deleted:    contact/script.js 	deleted:    contact/style.css 	deleted:    index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	deleted:    script.js 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js 	deleted:    style.css

--- script.js
+++
@@ -1,6 +0,0 @@
-// JavaScript code for interactivity
-document.querySelector("h1").addEventListener("click", function() {
-    alert("Welcome to my personal website!");
-});
-
-// You can add more JavaScript to enhance your site's functionality.
