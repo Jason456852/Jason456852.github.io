f6658372f9b56ee29b2ad25b3fb38a071fb1348b
Jason456852 <86191461+Jason456852@users.noreply.github.com>
2023-11-05 09:00:36 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript code for interactivity
+document.querySelector("h1").addEventListener("click", function() {
+    alert("Welcome to my personal website!");
+});
+
+// You can add more JavaScript to enhance your site's functionality.
