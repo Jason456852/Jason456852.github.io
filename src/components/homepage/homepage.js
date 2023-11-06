d179ba9bb2195f006ec07171affc742f98677762
Jason456852 <pakling.yeung456852@gmail.com>
2023-11-05 17:56:11 -0700

    modified:   src/components/homepage/homepage.js

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Homepage = () => {
     return (<>
         This is homepage.
+        <nav>
+            <ul>
+                <li>
+                    <Link to={`about`}>About</Link>
+                </li>
+                <li>
+                    <Link to={`contact`}>Contact</Link>
+                </li>
+            </ul>
+        </nav>
     </>);
 }
 
