88a2f85aa85b58819ba063bdf71dff67d01d162f
Jason456852 <pakling.yeung456852@gmail.com>
2023-11-05 18:00:53 -0700

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import Homepage from "./components/homepage/homepage";
-import About from "./components/about/about";
-import Contact from "./components/contact/contact";
+import Loading from "./components/loading.js";
 
-// const Homepage = lazy(() => import("./components/homepage/homepage.js"));
-// const About = lazy(() => import("./components/about/about.js"));
-// const Contact = lazy(() => import("./components/contact/contact.js"));
-
+const Homepage = lazy(() => import("./components/homepage/homepage.js"));
+const About = lazy(() => import("./components/about/about.js"));
+const Contact = lazy(() => import("./components/contact/contact.js"));
 
 const App = () => {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Homepage />}></Route>
-                <Route path="/about" element={<About />}></Route>
-                <Route path="/contact" element={<Contact />}></Route>
+                <Route path="/" element={<Suspense fallback={<Loading />}><Homepage /></Suspense>}></Route>
+                <Route path="/about" element={<Suspense fallback={<Loading />}><About /></Suspense>}></Route>
+                <Route path="/contact" element={<Suspense fallback={<Loading />}><Contact /></Suspense>}></Route>
             </Routes>
         </>
     );
