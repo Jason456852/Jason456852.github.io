ea1960bf17080cb895cee39135f4b6793029de43
Jason456852 <pakling.yeung456852@gmail.com>
2023-11-05 18:05:02 -0700

    Fix URL client routing not working in github

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import Loading from "./components/loading.js";
+import { Link } from "react-router-dom";
 
 const Homepage = lazy(() => import("./components/homepage/homepage.js"));
 const About = lazy(() => import("./components/about/about.js"));
@@ -9,6 +10,14 @@ const Contact = lazy(() => import("./components/contact/contact.js"));
 const App = () => {
     return (
         <>
+            <ul>
+                <li>
+                    <Link to={`about`}>About</Link>
+                </li>
+                <li>
+                    <Link to={`contact`}>Contact</Link>
+                </li>
+            </ul>
             <Routes>
                 <Route path="/" element={<Suspense fallback={<Loading />}><Homepage /></Suspense>}></Route>
                 <Route path="/about" element={<Suspense fallback={<Loading />}><About /></Suspense>}></Route>
