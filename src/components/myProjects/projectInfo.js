9673d3b8b095ab7614cf935a809481277cfa5fc4
canadianrefrigeration <canadianrefrigeration@telus.net>
2024-04-15 09:10:57 -0700

    Update

---
+++ src/components/myProjects/projectInfo.js
@@ -0,0 +1,11 @@
+export const projectInfo = [
+    {
+        name: "Canadian Refrigeration Simple Homepage",
+        description: "This is a very simple webpage for me to test myself of the skills and knowledge of basic webpage building. After I built the web page with HTML, CSS and JavaScript, I learned and added TailwindCSS.",
+        languages: "HTML, CSS, JavaScript, TailwindCSS",
+        link: {
+            isIframe: true,
+            src: "https://canadianrefrigeration.github.io"
+        }
+    },
+];
